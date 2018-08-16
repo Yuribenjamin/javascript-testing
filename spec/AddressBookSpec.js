81f154f6841b86ba4fb3feb9f0bf3783a96e4820
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2018-08-16 13:20:36 +0200

    refactoring

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,15 +1,22 @@
 describe('Address Book', function() {
+    let adressBook,
+        thisContact;
+
+    beforeEach(function(){
+        adressBook = new AddressBook();
+        thisContact = new Contact();
+    });
+
     it('should be able to add a contact', function(){
-        const adressBook = new AddressBook();
-            thisContact = new Contact();
             adressBook.addContact(thisContact);
+
             expect(adressBook.getContact(0)).toBe(thisContact);
     });
+
     it('should be able to delete a contact', function() {
-        const adressBook = new AddressBook();
-            thisContact = new Contact();
         adressBook.addContact(thisContact);
         adressBook.deleteContact(0);
+
         expect(adressBook.getContact(0)).not.toBeDefined();
     });
 });
